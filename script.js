002870219592ff07ca766cab0f2d5f995535133c
tekno <teknogyan123@gmail.com>
2023-03-15 18:03:14 +0530

    fist commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuClose = document.querySelector(".menu-close");
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu ul li a");
+console.log(menuItems);
+
+menuBar.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    menuBar.classList.toggle("active");
+    menuClose.classList.toggle("active");
+});
+
+menuClose.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    menuBar.classList.toggle("active");
+    menuClose.classList.toggle("active");
+});
+
+menuItems.forEach((item) =>
+    item.addEventListener("click", () => {
+        menu.classList.toggle("active");
+        menuBar.classList.toggle("active");
+        menuClose.classList.toggle("active");
+    })
+);
