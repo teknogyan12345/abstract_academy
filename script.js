77bebd74e277e1eb9c301b40efd189ab2c2ee0f4
tekno <teknogyan123@gmail.com>
2023-03-25 17:26:20 +0530

    ho gaya ab

--- script.js
+++ script.js
@@ -24,3 +24,29 @@ menuItems.forEach((item) =>
         menuClose.classList.toggle("active");
     })
 );
+
+
+const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 1400,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+    //   on: {
+    //     autoplayTimeLeft(s, time, progress) {
+    //       progressCircle.style.setProperty("--progress", 1 - progress);
+    //       progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    //     }
+    //   }
+    });
